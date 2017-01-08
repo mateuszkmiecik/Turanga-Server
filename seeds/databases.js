d777a3a9fe4f739c677a66a1a1e08ea89d2151ce
Marsuz <marcin.zajda@gmail.com>
2017-01-08 17:26:33 +0100

    [new] * added database stub to seeds

---
+++ seeds/databases.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mzajda on 08/01/2017.
+ */
+
+
+module.exports = [
+    {
+        name:"dvdrental",
+        dbEngine: "POSTGRESQL",
+        url: "jdbc:postgresql://localhost:5432/dvdrental",
+        user: "postgres",
+        password: "1234"
+    }
+]
+
